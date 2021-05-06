ec7c44a58d9a2504b007557a307f25cf263e7027
Dmitry Shevchuk <dmitriy_shevchuk_01@ukr.net>
2021-05-06 20:29:49 +0300

    stable version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// підключенння різних бібліотек та компонентів
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
+import React from 'react';
 
+import Login from './pages/Login';
+import Main from './pages/Main';
+import Reg from './pages/Reg';
+
+import useStyles from './styles';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, login } from './redux/actions/auth';
+
+//Головний компонент нашого додатку реалізований наступним чином
 function App() {
+  const classes = useStyles();
+  //отримання даних з Redux
+  const user = useSelector(({ login }) => login);
+  // створення хуку для маршрутизації
+  let history = useHistory();
+  const dispatch = useDispatch();
+
+  //авторизація
+  function onLogin(payload) {
+    dispatch(login(payload));
+    history.push('/main');
+  }
+  //вихід
+  function onLogout() {
+    dispatch(logout());
+  }
+  //console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Рендер головного компоненту
+    // маршрутизація
+    <Switch>
+      <Route path="/" exact>
+        {/* Перехід до авторизації */}
+        <Login onLogin={onLogin} />
+      </Route>
+      <Route path="/registration" exact>
+        {/* Перехід до регістрації */}
+        <Reg onLogin={onLogin} />
+      </Route>
+      <Route path="/main" exact>
+        {user.loggedIn ? (
+          <div>
+            {/* Перехід до головної */}
+            <Main user={user} onLogout={onLogout} />
+          </div>
+        ) : (
+          <Redirect to="/" />
+        )}
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+// підключення бібліотек
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { ThemeProvider } from '@material-ui/core';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* підключення "стору" з редакс*/}
+    <Provider store={store}>
+      {/* підключення маршрутизації */}
+      <Router>
+        {/* підключення головного компоненту і темізація компонентів*/}
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
